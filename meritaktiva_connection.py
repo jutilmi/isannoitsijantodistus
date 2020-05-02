023d59c9d3e504bb1bacc973ef5445d3ac43a315
mjutila <mikko.jutila@iki.fi>
2020-05-02 15:34:22 +0300

    Accounting class created and updated reporting module

---
+++ meritaktiva_connection.py
@@ -0,0 +1,54 @@
+'''Module includes methods for connecting and retrieving data in
+   accounting program API and retriving data from there.'''
+from datetime import datetime
+import json
+import hmac
+import hashlib
+from base64 import b64encode
+import requests
+
+class MeritAktiva():
+
+    '''Class for MeritAktiva API methods'''
+
+    def __init__(self, api_id: str, api_key: str):
+        '''Defining basic settings for class.
+           api_id: str, obtained in MeritAktiva company logged page
+           api_key: str, obtained in MeritAktiva company logged page
+           '''
+        self.api_id = api_id
+        self.api_key = api_key
+        self.entry_point = 'https://aktiva.meritaktiva.fi/api/v1/'
+
+    def get(self, api_address: str, api_arguments: dict)-> requests.Response:
+        """api_address: str, URL-address to api,
+        api_arguments: dict, arguments sent to api
+        returns: requests.Response object in json format"""
+
+        time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        json_request = json.dumps(api_arguments, separators=(',', ':'))
+
+        data_string = self.api_id + time_stamp + json_request
+        hashed = hmac.new(
+            bytes(self.api_key, 'utf-8'),
+            msg=bytes(data_string, 'utf-8'), digestmod=hashlib.sha256).digest()
+        signature = b64encode(hashed).decode('utf-8')
+
+        url = self.entry_point + api_address \
+            + '?ApiId=' + self.api_id \
+            + '&timestamp=' + time_stamp \
+            + '&signature=' + signature
+
+        response = requests.get(
+            url=url,
+            json=True,
+            headers={
+                'content-type': 'application/json'
+            },
+            data=json_request
+            )
+
+        return response
+
+    def push(self):
+        '''Method pushes data via MeritAktiva API'''
--- reports.py
+++ reports.py
@@ -11,53 +11,55 @@ def export_apartment_loan_share_calculation_as_html(
         loan_shares_sum: float,
         apartment_id: int,
         apartment_loan_share: float,
-        export_file: str):
+        export_file: str,
+        loan_description: str):
     """Function outputs an apartment loan share calculation in html format.
 
        Keyword arguments
        income_of_accounting_period           -- decimal.Decimal, income of existing\
-           accounting period
+           accounting period, always positive
            (invoiced charge amount, invoiced loan shares amount, other income)
         expense_of_accounting_period         -- decimal.Decimal, expenses of existing\
-            accounting period
+            accounting period, always negative
            (intereset expenses, other capital expenses, loan reductions)
        surplus_or_deficit_of_previous_accounting_period -- currency surplus/decifit
-           from previous accounting period
-       debt_capital_remaining                -- remaining capital for the loan
+           from previous accounting period, positive value surplus, degative decifit
+       debt_capital_remaining                -- remaining capital for the loan, always positive
        loan_shares_sum                       -- sum of all loan shares for the loan
        apartment_id                          -- apartment id where to calculation will be done
        apartment_loan_share                  -- amount of loan shares for apartment
        export_file                           -- path to file where the report will be created
+       loan_description                      -- description of the loan to be printed
        """
 
     income_sum = sum(income_of_accounting_period)
     expense_sum = sum(expense_of_accounting_period)
 
-    debt_in_total = income_sum\
+    debt_in_total = - income_sum\
         + expense_sum\
-        + surplus_or_deficit_of_previous_accounting_period\
+        - surplus_or_deficit_of_previous_accounting_period\
         + debt_capital_remaining
 
     loan_share_per_unit = debt_in_total.__float__() / loan_shares_sum
     loan_share_for_apartment = loan_share_per_unit * apartment_loan_share
 
-    income_sum_s = Decimal(sum(income_of_accounting_period)).\
-        __round__(2).__str__().replace('.', ',')
-    expense_sum_s = Decimal(sum(expense_of_accounting_period)).\
-        __round__(2).__str__().replace('.', ',')
-    surplus_or_deficit_s = Decimal(
-        surplus_or_deficit_of_previous_accounting_period).\
-            __round__(2).__str__().replace('.', ',')
-    debt_capital_remaining_s = Decimal(debt_capital_remaining).\
-        __round__(2).__str__().replace('.', ',')
-    debt_in_total_s = Decimal(debt_in_total).\
-        __round__(2).__str__().replace('.', ',')
+    income_sum_s = ' '.join((Decimal(sum(income_of_accounting_period)).\
+        __round__(2).__str__().replace('.', ','), '€'))
+    expense_sum_s = ' '.join((Decimal(sum(expense_of_accounting_period)).\
+        __round__(2).__str__().replace('.', ','), '€'))
+    surplus_or_deficit_s = ' '.join((Decimal(
+        -surplus_or_deficit_of_previous_accounting_period).\
+            __round__(2).__str__().replace('.', ','), '€'))
+    debt_capital_remaining_s = ' '.join((Decimal(debt_capital_remaining).\
+        __round__(2).__str__().replace('.', ','), '€'))
+    debt_in_total_s = ' '.join((Decimal(debt_in_total).\
+        __round__(2).__str__().replace('.', ','), '€'))
     loan_shares_sum_s = Decimal(loan_shares_sum).\
         __round__(2).__str__().replace('.', ',')
-    loan_share_per_unit_s = Decimal(loan_share_per_unit).\
-        __round__(2).__str__().replace('.', ',')
-    loan_share_for_apartment_s = Decimal(loan_share_for_apartment).\
-        __round__(2).__str__().replace('.', ',')
+    loan_share_per_unit_s = ' '.join((Decimal(loan_share_per_unit).\
+        __round__(2).__str__().replace('.', ','), '€'))
+    loan_share_for_apartment_s = ' '.join((Decimal(loan_share_for_apartment).\
+        __round__(2).__str__().replace('.', ','), '€'))
 
     h_str = '<!DOCTYPE html>\n'
     h_str += '<html lang="fi">\n'
@@ -66,7 +68,7 @@ def export_apartment_loan_share_calculation_as_html(
     h_str += '</head>\n'
     h_str += '<body>\n'
 
-    h_str += '<h1>Lainaosuuslaskelma</h1>\n'
+    h_str += '<h1>Lainaosuuslaskelma lainalle' + loan_description + '</h1>\n'
     h_str += ('<p>Päiväys: ' + dt.now().strftime('%Y-%m-%d') + '\n')
 
     h_str += '<h2>TILIKAUDEN TUOTOT</h2>\n'
@@ -74,16 +76,22 @@ def export_apartment_loan_share_calculation_as_html(
     h_str += '<tr>\n'
     h_str += '<td>Pääomavastikkeet</td>\n'
     h_str += '<td style="text-align:right">'\
-        + income_of_accounting_period[0].__round__(2).__str__().replace('.', ',') + '</td>\n'
+        + ' '.join((
+            income_of_accounting_period[0].__round__(2).\
+                __str__().replace('.', ','), '€')) + '</td>\n'
     h_str += '<tr>\n'
     h_str += '<td>Lainaosuussuoritukset</td>\n'
     h_str += '<td style="text-align:right">'\
-        + income_of_accounting_period[1].__round__(2).__str__().replace('.', ',') + '</td>\n'
+        + ' '.join((
+            income_of_accounting_period[1].__round__(2).\
+                __str__().replace('.', ','), '€')) + '</td>\n'
     h_str += '</tr>\n'
     h_str += '<tr>\n'
     h_str += '<td>Muut tuotot</td>\n'
     h_str += '<td style="text-align:right">'\
-        + income_of_accounting_period[2].__round__(2).__str__().replace('.', ',') + '</td>\n'
+        + ' '.join((
+            income_of_accounting_period[2].__round__(2).\
+                __str__().replace('.', ','), '€')) + '</td>\n'
     h_str += '</tr>\n'
     h_str += '<tr>\n'
     h_str += '<td><b>Tuotot yhteensä</b></td>\n'
@@ -97,16 +105,22 @@ def export_apartment_loan_share_calculation_as_html(
     h_str += '<tr>\n'
     h_str += '<td>Korkokulut</td>\n'
     h_str += '<td style="text-align:right">'\
-        + expense_of_accounting_period[0].__round__(2).__str__().replace('.', ',') + '</td>\n'
+        + ' '.join(
+            (expense_of_accounting_period[0].__round__(2).\
+                __str__().replace('.', ','), '€')) + '</td>\n'
     h_str += '<tr>\n'
     h_str += '<td>Lainanlyhennykset</td>\n'
     h_str += '<td style="text-align:right">'\
-        + expense_of_accounting_period[2].__round__(2).__str__().replace('.', ',') + '</td>\n'
+        + ' '.join(
+            (expense_of_accounting_period[2].__round__(2).\
+                __str__().replace('.', ','), '€')) + '</td>\n'
     h_str += '</tr>\n'
     h_str += '<tr>\n'
     h_str += '<td>Muut rahoituskulut</td>\n'
     h_str += '<td style="text-align:right">'\
-        + expense_of_accounting_period[1].__round__(2).__str__().replace('.', ',') + '</td>\n'
+        + ' '.join(
+            (expense_of_accounting_period[1].__round__(2).\
+                __str__().replace('.', ','), '€')) + '</td>\n'
     h_str += '</tr>\n'
     h_str += '<tr>\n'
     h_str += '<td><b>Kulut yhteensä</b></td>\n'
@@ -115,26 +129,52 @@ def export_apartment_loan_share_calculation_as_html(
     h_str += '</tr>\n'
     h_str += '</table>\n'
 
-    h_str += '<p>EDELLISELTÄ KAUDELTA SIIRTYVÄ PÄÄOMAVASTIKE YLIJÄÄMÄ\
-        ALIJÄÄMÄ: ' + surplus_or_deficit_s + '\n'
-
-    h_str += ('<p>LAINAPÄÄOMA: ' + debt_capital_remaining_s + '\n')
-    h_str += ('<p>LAINARASITUS YHTEENSÄ: ' + debt_in_total_s + '\n')
-
-    h_str += ('<p>LAINASTA VASTAAVAT YKSIKÖT: ' + loan_shares_sum_s + '\n')
-    h_str += ('<p>LAINARASITUS PER VELALLINEN YKSIKKÖ: ' + loan_share_per_unit_s + '\n')
-
-    h_str += ('<h2>HUONEISTON ' + apartment_id.__str__() + ' LAINAOSUUS</h2>\n')
-
-    h_str += '<table style="width:40%">\n'
+    h_str += '<table style="width:40%" border="1">\n'
     h_str += '<tr>\n'
+    h_str += '<td style="width:70%">LAINAPÄÄOMA TILIKAUDEN ALUSSA</td>\n'
+    h_str += '<td style="text-align:right;width:30%">' + debt_capital_remaining_s + '</td>\n'
+    h_str += '</tr>\n'
+    h_str += '<tr>\n'
+    h_str += '<td>EDELLISELTÄ KAUDELTA SIIRTYVÄ PÄÄOMAVASTIKKEEN\
+        YLIJÄÄMÄ(-)/ALIJÄÄMÄ(+)</td>\n'
+    h_str += '<td style="text-align:right">' + surplus_or_deficit_s + '</td>\n'
+    h_str += '</tr>\n'
+    h_str += '<tr>\n'
+    h_str += '<td>LAINARASITUS YHTEENSÄ LASKENTAHETKELLÄ</td>\n'
+    h_str += '<td style="text-align:right">' + debt_in_total_s + '</td>\n'
+    h_str += '</tr>\n'
+    h_str += '<tr>\n'
+    h_str += '<td>LAINASTA VASTAAVAT YKSIKÖT</td>\n'
+    h_str += '<td style="text-align:right">' + loan_shares_sum_s + '</td>\n'
+    h_str += '</tr>\n'
+    h_str += '<tr>\n'
+    h_str += '<td>LAINARASITUS PER VELALLINEN YKSIKKÖ LASKENTAHETKELLÄ</td>\n'
+    h_str += '<td style="text-align:right">' + loan_share_per_unit_s + '</td>\n'
+    h_str += '</tr>\n'
+    h_str += '</table>\n'
+    #h_str += '<p>EDELLISELTÄ KAUDELTA SIIRTYVÄ PÄÄOMAVASTIKE\
+    #    YLIJÄÄMÄ(-)/ALIJÄÄMÄ(+): ' + surplus_or_deficit_s + '\n'
+
+    #h_str += ('<p>LAINAPÄÄOMA: ' + debt_capital_remaining_s + '\n')
+    #h_str += ('<p>LAINARASITUS YHTEENSÄ: ' + debt_in_total_s + '\n')
+
+    #h_str += ('<p>LAINASTA VASTAAVAT YKSIKÖT: ' + loan_shares_sum_s + '\n')
+    #h_str += ('<p>LAINARASITUS PER VELALLINEN YKSIKKÖ: ' + loan_share_per_unit_s + '\n')
+
+    h_str += ('<h2>HUONEISTOLLE KOHDISTUVA LAINAOSUUS LASKENTAHETKELLÄ</h2>\n')
+
+    h_str += '<table style="width:40%" border="1">\n'
+    h_str += '<tr>\n'
+    h_str += '<th>Huoneiston numero</th>\n'
     h_str += '<th>Huoneiston lainayksikot</th>\n'
     h_str += '<th>Lainaosuus per yksikko</th>\n'
     h_str += '<th>Lainaosuus huoneistolle</th>\n'
     h_str += '</tr>\n'
     h_str += '<tr>\n'
     h_str += '<td style="text-align:right">'\
-        + apartment_loan_share.__round__(6).__str__() + '</td>\n'
+        + apartment_id.__str__() + '</td>\n'
+    h_str += '<td style="text-align:right">'\
+        + apartment_loan_share.__round__(6).__str__().replace('.', ',') + '</td>\n'
     h_str += '<td style="text-align:right">'\
         + loan_share_per_unit_s + '</td>\n'
     h_str += '<td style="text-align:right"><b>'\
